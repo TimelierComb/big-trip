e03ab630deaa018dde002e3abaa6420f009665f9
timeliercomb <levshin.v.99@gmail.com>
2022-09-13 16:46:05 +0300

    выносит функцию рендера в модуль утилит, добавляет функции генерации и рендера ДОМ элементов, начинает переход от функций к классам

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,40 @@
 import dayjs from 'dayjs';
 
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+export const render = (container, template, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
--- src/view/trip-sort.js
+++ src/view/trip-sort.js
@@ -1,3 +1,5 @@
+import {createElement} from "../utils.js";
+
 export const createTripSortTemplate = () => `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
 <div class="trip-sort__item  trip-sort__item--day">
   <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
@@ -26,3 +28,23 @@ export const createTripSortTemplate = () => `<form class="trip-events__trip-sort
 </form>
 
 <ul class="trip-events__list"></ul>`;
+
+// export default class TripSortView {
+//   #element = null;
+
+//   get template() {
+//     return createTripSortTemplate;
+//   }
+
+//   get element() {
+//     if (!this.#element) {
+//       this.#element = createElement(this.template);
+//     }
+
+//     return this.#element;
+//   }
+
+//   removeElement() {
+//     this.#element = null;
+//   }
+// }
